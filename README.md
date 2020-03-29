34508933cc339982946de02736059f776544caac
Martuza <martuza.shimul@gmail.com>
2020-03-30 02:30:47 +0600

    searchParams component added and eslint configured for react-hooks

--- README.md
+++ README.md
@@ -8,8 +8,15 @@
 - Tools
   - npm & Generating a package.json file
   - Prettier
+    - Prettier config
   - npm Scripts
   - Eslint
+    - eslint config for react
 - Parcel Bundler
 - JSX
--
+- Hooks
+  - setting state with hooks
+  - Best practices for hooks
+    - never use a hook inside any statement like conditions or loop
+  - Configuring ESLint for hooks
+    - npm i -D eslint-plugin-react-hooks
---
+++ src/SearchParams.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const SearchParams = () => {
+  // const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
