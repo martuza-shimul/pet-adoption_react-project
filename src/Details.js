c5387b1f520a9543a939885fadd888d1a8cb8d04
Martuza <martuza.shimul@gmail.com>
2020-03-31 19:19:08 +0600

    image carousel added to the details page

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   state = {
@@ -24,9 +25,10 @@ class Details extends Component {
       return <h1>Loading ...</h1>;
     }
 
-    const { animal, breed, location, description, name } = this.state;
+    const { animal, breed, location, description, name, media } = this.state;
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
